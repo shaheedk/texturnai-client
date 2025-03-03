a48d97d03dd7dd97667b51deff1138dde119fe57
Shaheed mon <sheheedk07@gmail.com>
2025-03-03 22:59:09 +0530

    commit

--- src/components/Descreption.jsx
+++ src/components/Descreption.jsx
@@ -1,20 +1,38 @@
-import { assets } from "../assets/assets"
-
+import { assets } from "../assets/assets";
 
 const Descreption = () => {
   return (
     <div className="flex flex-col items-center justify-center my-24 p-6 md:px-28">
-      <h1 className="text-3xl sm:text-4xl font-semibold mb-2">Create AI Images</h1>
+      <h1 className="text-3xl sm:text-4xl font-semibold mb-2">
+        Create AI Images
+      </h1>
       <p className="text-gray-500 mb-8">Trun you imagination into visuals</p>
-      <div>
-        <img src={assets.sample_img_1} alt="" className="w-80 xl:w-96 rounded-lg"/>
-        <div>
-            <h2>Introducing the Ai-Powered Text to Image Generator</h2>
-            <p>Easily bring your ideas to life with our free AI image Generator. Whether you need stunning visuals or unique imagery ,our tool transforms your text into eye-catching images with jsut a few clocks.Imagine it, describe it,images with just a few clicks.Imagine it,describe it,and watch it come to life instrantly.</p>
+      <div className="flex flwx-col gap-5 md:gap-14 md:flex-row items-center">
+        <img
+          src={assets.sample_img_1}
+          alt=""
+          className="w-80 xl:w-96 rounded-lg"
+        />
+        <div >
+          <h2 className="t-3xl font-medium max-w-lg">Introducing the Ai-Powered Text to Image Generator</h2>
+          <p className="text-gray-600 mb-4">
+            Easily bring your ideas to life with our free AI image generator.
+            Whether you need stunning visuals or unique imagery, our tool
+            transforms your text into eye-catching images with just a few
+            clicks. Imagine it, describe it, and watch it come to life
+            instantly.
+          </p>
+          <p className="text-gray-600">
+            Simply type in a text prompt, and our cutting-edge AI will generate
+            high-quality images in seconds. From product visuals to character
+            designs and portraits, even concepts that don't yet exist can be
+            visualized effortlessly. Powered by advanced AI technology, the
+            creative possibilities are limitless.
+          </p>
         </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Descreption
+export default Descreption;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../components/Header'
 import Steps from '../components/Steps'
 import Descreption from '../components/Descreption'
+import Testmonials from '../components/Testmonials'
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
       <Header/>
       <Steps/>
       <Descreption/>
+      <Testmonials/>
     </div>
   )
 }
