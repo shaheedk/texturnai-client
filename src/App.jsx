e52dd23f703065c5db1f5bfc41df9340e08eeeac
Shaheed mon <sheheedk07@gmail.com>
2025-03-06 23:53:46 +0530

    designs ongoing

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import NotFound from "./pages/NotFound";
+import Login from "./components/Login";
 const App = () => {
   return (
     <div className="px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50">
       <NavBar />
+      <Login/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/result" element={<Result />} />
--- src/components/GenerateButton.jsx
+++ src/components/GenerateButton.jsx
@@ -3,14 +3,16 @@ import { assets } from "../assets/assets";
 
 const GenerateButton = () => {
   return (
+    
     <div className="pb-16 text-center">
       <h1 className="text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold text-neutral-800 py-6">
         See the magic.Try now
       </h1>
       <Link to='/result' className="inline-flex items-center gap-2 px-12 py-3 rounded-full bg-black text-white m-auto hover:scale-105 transition-all duration-500">
         Generate Images
-      
+  
       <img src={assets.star_group} alt="" className="h-6" />
+      
       </Link>
     </div>
   );
