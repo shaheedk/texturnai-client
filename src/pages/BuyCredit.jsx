a46608a75ebdeb3b25e1848a13bdfc5ed2e5dc1b
Shaheed mon <sheheedk07@gmail.com>
2025-03-06 06:17:19 +0530

    ongoing design

--- src/pages/BuyCredit.jsx
+++ src/pages/BuyCredit.jsx
@@ -1,30 +1,31 @@
-import React from 'react'
-import { assets, plans } from '../assets/assets'
+import React from "react";
+import { assets, plans } from "../assets/assets";
 
 function BuyCredit() {
   return (
     <div className="min-h-[80vh] text-center mt-14 mb-10">
-      <button className='border border-gray-400 px-10 py-2 rounded-full mb-6'>
+      <button className="border border-gray-400 px-10 py-2 rounded-full mb-6">
         Our plans
       </button>
-      <h1 className='text-center text-3xl font-medium mb-6 sm:mb-10'>
+      <h1 className="text-center text-3xl font-medium mb-6 sm:mb-10">
         Choose the plan
       </h1>
 
-
-      <div>
-        {plans.map((item,index)=>(
-        <div key={index}>
-          <img src={assets.lock_icon} alt="" />
-          <p>{item.id}</p>
-          <p>{item.dese}</p>
-          <p>{item.price} /{item.credits} credits</p>
-
-        </div>  
+      <div className="flex flex-wrap justify-center gap-6 text-left">
+        {plans.map((item, index) => (
+          <div key={index}
+           className="">
+            <img src={assets.logo_icon} alt="" />
+            <p>{item.id}</p>
+            <p>{item.dese}</p>
+            <p>
+              ${item.price} /{item.credits} credits
+            </p>
+          </div>
         ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default BuyCredit
+export default BuyCredit;
