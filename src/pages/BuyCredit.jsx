a007a1d486f0b47c9f7925dd956034c4d83e61b1
Shaheed mon <sheheedk07@gmail.com>
2025-04-04 00:06:44 +0530

    done

--- src/pages/BuyCredit.jsx
+++ src/pages/BuyCredit.jsx
@@ -3,33 +3,69 @@ import { assets, plans } from "../assets/assets";
 import { AppContext } from "../context/AppContext";
 import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import axios from "axios";
 function BuyCredit() {
-  const { user,backendUrl,loadCreditData,token,setShowLogin } = useContext(AppContext);
-  const navigate=useNavigate()
+  const { user, backendUrl, loadCreditData, token, setShowLogin } =
+    useContext(AppContext);
+  const navigate = useNavigate();
 
-  const initPay=async(order)=>{
-
-  }
-  const paymentRazorpay=async(planId)=>{
-    try {
-      if(!user){
-        setShowLogin(true)
+  const initPay = async (order) => {
+    const options = {
+      key: import.meta.env.VITE_RAZORPAY_KEY_ID,
+      amount: order.amount,
+      currency: order.currency,
+      name: "Credits Payment",
+      description: "Credits Payment",
+      order_id: order.id,
+      receipt: order.receipt,
+      handler: async (response) => {
+        try {
+          const { data } = await axios.post(
+            backendUrl + "/api/user/verify-razor",
+            response,
+            { headers: { token } }
+          );
+          
+          if (data.success) {
+            loadCreditsData();
+            navigate("/");
+            toast.success("Credit Added");
+          }
+        } catch (error) {
+          toast.error(error.message);
+        }
       }
-     const {data}= await axios.post(backendUrl+'/api/user/pay-razor',{planId},{headers:{token}})
-     if(data.success){
-initPay(data.order)
-     }
+      
+    };
+    const rzp = new window.Razorpay(options);
+    rzp.open();
+  };
+
+  const paymentRazorpay = async (planId) => {
+    try {
+      if (!user) {
+        setShowLogin(true);
+      }
+      const { data } = await axios.post(
+        backendUrl + "/api/user/pay-razor",
+        { planId },
+        { headers: { token } }
+      );
+      if (data.success) {
+        initPay(data.order);
+      }
     } catch (error) {
-      toast.error(error.message)
+      toast.error(error.message);
     }
-  }
+  };
   return (
-    <motion.div className="min-h-[80vh] text-center mt-14 mb-10"
-    initial={{ opacity: 0.2, y: 100 }}
-    transition={{ duration: 1 }}
-    whileInView={{ opacity: 1, y: 0 }}
-    viewporrt={{ once: true }}
-  
+    <motion.div
+      className="min-h-[80vh] text-center mt-14 mb-10"
+      initial={{ opacity: 0.2, y: 100 }}
+      transition={{ duration: 1 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewporrt={{ once: true }}
     >
       <button className="border border-gray-400 px-10 py-2 rounded-full mb-6">
         Our plans
@@ -51,7 +87,10 @@ initPay(data.order)
               <span className="text-3xl font-medium"> ${item.price}</span> /
               {item.credits} credits
             </p>
-            <button className=" w-full bg-gray-800 text-white mt-8 text-sm py-2.5 min-w-52 rounded-md">
+            <button
+              onClick={() => paymentRazorpay(item.id)}
+              className=" w-full bg-gray-800 text-white mt-8 text-sm py-2.5 min-w-52 rounded-md"
+            >
               {user ? "Purchase" : "Get Started"}{" "}
             </button>
           </div>
